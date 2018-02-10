fab5670a7427a473c92523598c950a1bc2c0a5c8
Dustin Hammack <rdhammack88@hotmail.com>
2018-02-10 01:57:59 -0600

    Updated

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bomber Skateboard Shop</title>
+	
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<header>
+		<p class="branding-ad"></p>
+		<h1 class="brand-logo">Bomber Skateboard Shop</h1>
+		<nav>
+			<ul>
+				<li>
+					<span>Skate</span>
+					<div>
+						<h4>Skateboard Setup</h4>
+						<ul>
+							<li>Completes</li>
+							<li>Decks</li>
+						</ul>
+						<h4>Skateboard Components</h4>
+						<ul>
+							<li>Wheels</li>
+							<li>Axles</li>
+							<li>Bearings</li>
+							<li>Hardware</li>
+						</ul>
+					</div>				
+				</li>
+				<li>
+					<span>Clothing</span>
+					<div>
+						<ul>
+							<li>Shop All</li>
+							<li>Jackets</li>
+							<li>Shirts</li>
+							<li>Pants</li>
+							<li>Belts</li>
+							<li>Socks</li>
+							<li>Shoes</li>
+						</ul>
+					</div>				
+				</li>
+			</ul>
+			<input type="search">
+		</nav>
+		</nav>
+	</header>
+</body>
+</html>
+
+
+<!-- 
+	<span class="glyphicons glyphicons-skateboard"></span>
+	<span class="glyphicons glyphicons-402-skateboard.png"></span>
+	<span class="glyphicons glyphicons-290-bomb.png"></span>
+	<span class="glyphicons glyphicons-28-search.png"></span>
+  -->
