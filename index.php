5f8cfb16a70de4088cfa7c7573941ac2eaa83c15
Dustin Hammack <rdhammack88@hotmail.com>
2018-02-14 01:55:32 -0600

    Updated

--- index.php
+++ index.php
@@ -8,56 +8,137 @@
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
+
+	<noscript>
+        <div>
+            <div>
+                <p><strong>JavaScript seems to be disabled in your browser.</strong></p>
+                <p>You must have JavaScript enabled in your browser to utilize the functionality of this website.</p>
+			</div>
+		</div>
+    </noscript>
+
 	<div class="branding-ad">
 		<p>Branding Content Goes Here!</p>
 		<span class="close">&times;</span>
 	</div>
 	<header>
-		<h2 class="brand-logo">Bomber Skateboard Shop</h2>
+		<div class="logo-container">
+			<h2 class="brand-logo"><i class="fas fa-bomb"></i> Bomber Skate Shop</h2>
+		</div>
+		<div class="promos-container">
+			<div class="promo-block">
+				<p class="promo-title">Free Shipping</p>
+				<p class="promo-text">On orders $50+ ($3.95 otherwise)</p>
+			</div>
+			<div class="promo-block">
+				<p class="promo-title">Free Returns</p>
+				<p class="promo-text">On Shoes, Clothing, and Accessories</p>
+			</div>
+			<div class="promo-block">
+				<p class="promo-title">No Sales Tax</p>
+				<p class="promo-text">On Anything!</p>
+			</div>
+		</div>
 		<nav>
 			<div class="nav-container">
 				<ul>
+					<li><a href="index.php">Home</a></li>
 					<li>
 						<a href="#"><span>Skate</span></a>
-						<div>
-							<h4>Skateboard Setup</h4>
+						<div id="skate-product-links">
+							<h5>Skateboard Setup</h5>
 							<ul>
-								<li>Completes</li>
-								<li>Decks</li>
+								<a href="?cat=complete"><li>Completes</li></a>
+								<a href="?cat=decks"><li>Decks</li></a>
 							</ul>
-							<h4>Skateboard Components</h4>
+							<h5>Skateboard Components</h5>
 							<ul>
-								<li>Wheels</li>
-								<li>Axles</li>
-								<li>Bearings</li>
-								<li>Hardware</li>
+								<a href="?cat=wheels"><li>Wheels</li></a>
+								<a href="?cat=axles"><li>Trucks</li></a>
+								<a href="?cat=bearings"><li>Bearings</li></a>
+								<a href="?cat=hardware"><li>Hardware</li></a>
 							</ul>
 						</div>				
 					</li> <!-- Skate list item -->
 					<li>
 						<a href="#"><span>Clothing</span></a>
-						<div>
+						<div id="skate-clothing-links">
 							<ul>
-								<li>Shop All</li>
-								<li>Jackets</li>
-								<li>Shirts</li>
-								<li>Pants</li>
-								<li>Belts</li>
-								<li>Socks</li>
-								<li>Shoes</li>
+								<a href="?cat=shop_all"><li>Shop All</li></a>
+								<a href="?cat=jackets"><li>Jackets</li></a>
+								<a href="?cat=shirts"><li>Shirts</li></a>
+								<a href="?cat=pants"><li>Pants</li></a>
+								<a href="?cat=belts"><li>Belts</li></a>
+								<a href="?cat=socks"><li>Socks</li></a>
+								<a href="?cat=shoes"><li>Shoes</li></a>
 							</ul>
 						</div>				
 					</li> <!-- Clothing list item -->
+					<li><a href="accessories.php">Accessories</a></li>
+					<li><a href="brands.php">Brands</a></li>
+					<li><a href="sale.php">Sale</a></li>
+<!--
+					<li><a href="affiliate.php">Affiliates</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="contact.php">Contact</a></li>
+-->
 				</ul> <!-- nav list -->
 				<div class="input-group">
-					<input type="search">
+					<input type="search" placeholder="Search">
 					<button type="button" class="btn input-group-btn"><span class="fas fa-search"></span></button>
 				</div>
 			</div> <!-- .nav-container -->
 		</nav> <!-- nav -->
 	</header> <!-- header -->
 	
+	<main>
 	
+		<div class="hero-image"></div>
+	
+		<div class="product-list">
+			<h3>Most Popular Categories</h3>
+			<ul>
+				<li>
+					<a href="?cat=shoes">
+						<img src="../skate-images/Clothing/Shoes/nike_sb_bruin_hyperfeel_canvas_nike_men_s_skateboarding_shoe.jpg" alt="Skate Shoes">
+						<span>Shoes</span>
+					</a>
+				</li>
+				<li>
+					<a href="?cat=decks">
+						<img src="../skate-images/Decks/dgk-josh-kalis-100-pure-skateboard-deck.jpg" alt="Skate Decks">
+						<span>Decks</span>
+					</a>
+				</li>
+				<li>
+					<a href="?cat=wheels">
+						<img src="../skate-images/Wheels/spitfire-skateboard-wheels.jpg" alt="Skate Wheels">
+						<span>Wheels</span>
+					</a>
+				</li>
+				<li>
+					<a href="?cat=shoes">
+						<img src="../skate-images/Clothing/Shoes/nike_sb_bruin_hyperfeel_canvas_nike_men_s_skateboarding_shoe.jpg" alt="Skate Shoes">
+						<span>Shoes</span>
+					</a>
+				</li>
+				<li>
+					<a href="?cat=decks">
+						<img src="../skate-images/Decks/dgk-josh-kalis-100-pure-skateboard-deck.jpg" alt="Skate Decks">
+						<span>Decks</span>
+					</a>
+				</li>
+				<li>
+					<a href="?cat=wheels">
+						<img src="../skate-images/Wheels/spitfire-skateboard-wheels.jpg" alt="Skate Wheels">
+						<span>Wheels</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+		
+	</main>
 	
 	
 	
@@ -81,4 +162,7 @@
 	<span class="glyphicons glyphicons-402-skateboard.png"></span>
 	<span class="glyphicons glyphicons-290-bomb.png"></span>
 	<span class="glyphicons glyphicons-28-search.png"></span>
+	
+	
+	http://localhost/projectPortfolio/projectRepos/Bomber-Skateshop/
   -->
