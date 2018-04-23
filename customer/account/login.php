ed917a0346091dd0fa6c219aaf0493dbc1a4afe9
Dustin Hammack <rdhammack88@hotmail.com>
2018-04-23 02:31:36 -0500

    Customer Directory added

--- customer/account/login.php
+++ customer/account/login.php
@@ -1,6 +1,6 @@
-<?php $page_title = 'Customer Profile'; ?>
+<?php $page_title = 'Customer Login'; ?>
 <?php $page_dir = substr(dirname(__FILE__), strrpos(dirname(__FILE__), '\\')+1); ?>
-<?php require_once('../includes/init.php'); ?>
+<?php require_once('../../includes/init.php'); ?>
 <?php require_once(SITE_ROOT.'/includes/header.php'); ?>
 
 
